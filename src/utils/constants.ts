e9a3688bf98f74671ee69f79f3cdda0fdb0ae457
Amaan Qureshi <git@amaanq.com>
2026-01-11 21:59:01 -0500

    NoProfileTheming: add plugin to hide Nitro profile customizations

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    amaanq: {
+        name: "amaanq",
+        id: 961290059171127356n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
